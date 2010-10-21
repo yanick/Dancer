d70f9c3b2f8f3f3d15b2961f73ff2b1b69557169
franck cuny <franck@lumberjaph.net>
2010-10-21 22:12:27 +0200

    refactoring:
    - no more Dancer::Headers (HTTP::Headers are stored in D::SharedData->headers)
    - default method to parse headers in D::Handlers

--- lib/Dancer/Handler.pm
+++ lib/Dancer/Handler.pm
@@ -3,6 +3,8 @@ package Dancer::Handler;
 use strict;
 use warnings;
 
+use HTTP::Headers;
+
 use Dancer::Logger;
 use Dancer::GetOpt;
 use Dancer::SharedData;
@@ -91,6 +93,18 @@ sub psgi_app {
     };
 }
 
+sub init_request_headers {
+    my ($self, $env) = @_;
+
+    my $psgi_headers = HTTP::Headers->new(
+        map {
+            (my $field = $_) =~ s/^HTTPS?_//;
+            ($field => $env->{$_});
+          }
+          grep {/^(?:HTTP|CONTENT|COOKIE)/i} keys %$env
+    );
+    Dancer::SharedData->headers($psgi_headers);
+}
 
 # render a PSGI-formated response from a response built by
 # handle_request()
